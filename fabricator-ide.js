62bf4ba361959534eda978b2fb06865eb35f0727
AravindIM <iamaravindim@gmail.com>
2019-06-28 20:27:09 +0530

    Initial design

---
+++ fabricator-ide.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+
+function mainWindow() {
+    let win = new BrowserWindow({
+        minWidth: 800,
+        minHeight: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+        frame: false
+    })
+
+    win.loadFile('mainwindow.html')
+
+    win.on('closed', () => {
+        win = null
+    })
+
+    win.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit()
+        }
+    })
+
+    win.on('activate', () => {
+        if (win === null) {
+            mainWindow()
+        }
+    })
+}
+
+app.on('ready', mainWindow)
